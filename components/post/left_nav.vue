eafe8a7b71f266b14c00053c47ba036763ff4258
yipgimhou <173808636@qq.com>
2020-01-06 16:49:29 +0800

    微信付款页面完成

---
+++ components/post/left_nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="left_nav">
+    <el-row type="flex" justify="space-between" class="nav">
+        <el-col :span="6" class="nav_item">
+            <span>热门城市</span>
+            <i class="el-icon-arrow-right"></i>
+            </el-col>  
+        <el-col :span="6" class="nav_item">热门城市</el-col>  
+        <el-col :span="6" class="nav_item">热门城市</el-col>  
+        <el-col :span="6" class="nav_item">热门城市</el-col>  
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.nav{
+    width: 261px;
+    height: 165px;
+    border:1px solid #dfdfdf;
+    flex-direction:column;
+    .nav_item{
+        // flex-direction:column;
+        margin-left:10px; 
+        line-height: 41px;
+        width: 250px;
+        
+    }
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>攻略模块</div>
+  <div>攻略模块
+    <LeftNav/>
+  </div>
 </template>
 
 <script>
+import LeftNav from "@/components/post/left_nav.vue"
 export default {
-
+components:{
+  LeftNav
+}
 }
 </script>
 
